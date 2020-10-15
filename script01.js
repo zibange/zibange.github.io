b65887ea837bc1c22d003bc3e8312d691ed3a306
zibange <1045093298@qq.com>
2020-10-15 21:38:12 +0800

    登录改进

--- script01.js
+++ script01.js
@@ -3,14 +3,13 @@ window.onload = wecomePage;
 function wecomePage() {
     var ans = prompt("这是一个私人网站，请输入密码：");
     if (ans == "123") {
-        alert("yes");
+        alert("欢迎您进入此网站");
+        window.location="easyForm.html"
     }
     else {
-        alert("no");
+        alert("你不能进入此网站");
         // 关闭当前页面，**注意必须是window.open()打开的页面才有效果**
-        window.opener = null;
-        window.open('', '_self');
-        window.close();
-
+        window.location = "byeBye.html";
+        return false;
     }
 }
